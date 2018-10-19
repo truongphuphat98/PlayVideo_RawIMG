f2300216aa3d38f5d4f96602cf9b3a2c0e0e0c8e
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2018-10-19 19:09:06 +0200

    PlayVideo_Canvas
    Play video on Canvas Raw Image

---
+++ PlayVideo_Canvas_RawImage/Assets/Video/PlayVideo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayVideo : MonoBehaviour
+{
+    public MovieTexture movie;
+    private AudioSource audio;
+
+    void Start()
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        audio = GetComponent<AudioSource>();
+        audio.clip = movie.audioClip;
+        movie.Play();
+        audio.Play();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && movie.isPlaying)
+        {
+            movie.Pause();
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && !movie.isPlaying)
+        {
+            movie.Play();
+        }
+    }
+}
